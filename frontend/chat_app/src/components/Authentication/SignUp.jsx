13243fa19bf021d84a91862bad64e11898d004ca
AkshayMamillapalli <akshaymamillapalli@gmail.com>
2026-07-27 04:49:12 +0530

    Fix CSS import case

--- frontend/chat_app/src/components/Authentication/SignUp.jsx
+++ frontend/chat_app/src/components/Authentication/SignUp.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import toast from "react-hot-toast";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "../../styles/Auth.css";
+import "../../styles/auth.css";
 
 function SignUp() {
   const [show, setShow] = useState(false);
