350a6950f55121e28d4845fc21e589db5b897f0b
AkshayMamillapalli <akshaymamillapalli@gmail.com>
2026-07-27 04:52:31 +0530

    Fix production static path

--- backend/server.js
+++ backend/server.js
@@ -28,16 +28,18 @@ app.use("/api/message", messageRoutes);
 app.use("/api/admin", adminRoutes);
 
 const __dirname1 = path.resolve();
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname1,"frontend","chat_app","dist")));
 
-    app.use((req, res) => {
-        res.sendFile(path.resolve(__dirname1, "frontend","chat_app", "dist", "index.html"));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname1, "../frontend/chat_app/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(
+            path.join(__dirname1, "../frontend/chat_app/dist/index.html")
+        );
     });
-}
-else{
-    app.get("/", (req,res) => {
-    res.send("API is Running Successfully");
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is Running Successfully");
     });
 }
 
